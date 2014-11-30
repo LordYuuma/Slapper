32087d5f6c68df99522d4760c03291a14ac84526
LordYuuma <leoprikler@aol.de>
2014-11-30 05:05:53 +0100

    Documentation Update. Otherwise filler.

--- README.md
+++ README.md
@@ -8,6 +8,14 @@ Installing:
 Run `(sudo) python setup.py install` to install the modules needed for this to work. Then move
 `slapper.py` into your hexchat plugin directory.
 
+Features:
+------
+* Slapping people in IRC.
+* Slapping *multiple* people at once.
+* Defining more than one command to be executed to slap someone.
+* Usage of long names (with spaces) for both slappers and targets (with escaping!)
+* Defining multiple slappers. (Never write more than one plugin)
+
 Usage:
 ------
 To slap one (or multiple) target(s) use `/slap <target(s)>`. All of your targets will then be
@@ -15,3 +23,18 @@ slapped (one slap statement for everyone to prevent spamming). To specify a slap
 not a trout, use `/slap -s <slapper> <target(s)>`. Slapper and targets are seperated by spaces,
 but you can quote them or escape the spaces to specify a longer name. Whenever you feel like
 needing help use `/slap --help`.
+
+Slapper Configuration:
+------
+The following keys are used and configurable for each Slapper:
+* `object`: the name of the object used to slap someone with
+* `count`: the usage count. (This is mostly used for the internal incrementation, but we let you have fun with it)
+* `target_format`: use HexChat codes for formatting the names of your targets.
+* `and`: for localization when targetting multiple users. For example *Foo and Bar* sounds perfectly fine in English, but for the use in a german sentence it should be reworded *Foo und Bar*.
+
+The commands in the commands section use Python format strings, where the following replacments
+are made:
+* `{object}` is replaced with the object
+* `{targets}` is replaced with the targets you slapped
+* `{count}` is replaced with the usage count of your slapper
+* `{count_ordinal}` is replaced with the (english) ordinal of your usage count
--- src/hc_slapper.py
+++ src/hc_slapper.py
@@ -14,28 +14,33 @@
 # You should have received a copy of the GNU General Public License
 # along with this program. If not, see <http://www.gnu.org/licenses/>.
 
-
 import hexchat
 from ast import literal_eval as literal
 from ConfigParser import ConfigParser, NoOptionError
 from os.path import join
 
+# safe commands used within IRC for saying stuff
 CS_SAFE = ["me","say"]
 
+# section names used internally
 S_COMMANDS = "commands"
 S_SLAPPER = "slapper"
 
+# key names used internally
 K_AND = "and"
 K_COUNT = "count"
 K_OBJECT = "object"
 K_TFMT = "target_format"
 
+# preference keys
 P_CFGDIR = "slapper_cfg_dir"
 
+# defaults
 D_CFGDIR = "slapper"
 D_CMDKEY = "default"
 D_CMDSLAP = "me slaps \002{target}\002 with {object}."
 
+# file endings
 FE_SLAPPER = ".slapper"
 
 # sets plugin preference if not already existing
@@ -71,12 +76,13 @@ class SelfUpdatingSection(object):
 
 class Slapper(ConfigParser):
 
-
     __getitem__ = lambda self, section: SelfUpdatingSection(section, self, self.file)
     __iter__ = lambda self: (self[section] for section in self.sections())
     __repr__ = lambda self: "{" + ", ".join(["\'{}\': {}".format(section, repr(self[section])) for section in self.sections()]) + "}"
 
     def check_sanity(self):
+        # This just checks, if every option, that needs to exist, also exists properly.
+        # This should only be called by the user, if he thinks, he has REALLY messed up.
         if not self.has_section(S_SLAPPER): self.add_section(S_SLAPPER)
         if not self.has_option(S_SLAPPER, K_OBJECT): self.set(S_SLAPPER, K_OBJECT, self.name)
         if not self.has_option(S_SLAPPER, K_COUNT): self.set(S_SLAPPER, K_COUNT, 0)
@@ -92,6 +98,10 @@ class Slapper(ConfigParser):
         if not self.read(self.file): self.check_sanity()
 
     def parse_targets(self, targets):
+        """
+        Takes a list of targets and formats them according to the slapper's formatting options
+        to return a string, that looks like something you can write as part of a sentence.
+        """
         try:
             t_fmt = self[S_SLAPPER][K_TFMT]
         except NoOptionError:
@@ -108,9 +118,16 @@ class Slapper(ConfigParser):
 
 
     def slap(self, targets):
-        self.read(self.file) # important! a value may have updated
+        # update all the settings from the file.
+        # this is important, because a new value may have been set and normally, one does direct
+        # file editing for a reason.
+        self.read(self.file)
+        # This increment's the slapper's use count and also sets it again.
+        # Since we use self updating sections, this change is automatically written to the file.
         count = int(self[S_SLAPPER][K_COUNT]) + 1
         self[S_SLAPPER][K_COUNT] = count
+        # The following formats the command according to the formatting specs defined, and
+        # executes those, that are safely usable.
         for command in sorted(self[S_COMMANDS]):
             cmd = literal("'" + command[1] + "'").format(name = self.name,
                                                          targets = self.parse_targets(targets),
